b9705a4ef67579623d88dd7f265778b36c99db6b
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-16 20:05:00 +0100

    Add task 1 (create random hex-color)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-export default function App() {
-  
+import { useState } from 'react';
+
+export default function ExampleState() {
+  // 1. Declare a state variable called "count", with
+  // the default value 0
+  const [hexcolor, generateHexColor] = useState("#fffff");
+  const randomHex = "#" + Math.floor(Math.random()*167777215 +' ' ).toString(16)
   return (
-    <div>
-      <h1>Random Color Generator</h1>
-      
-    </div>
+    <>
+      <h1>Color Generator</h1>
+      {/* 2. Use the state variable */}
+      <div>
+        Generated Color: {hexcolor}
+        </div>
+      <br></br>
+      <button
+        onClick={() =>
+          // 3. Set the state variable to a new value
+          generateHexColor(randomHex)
+        }
+      >
+        Generate
+      </button>
+   
+    </>
   );
 }
-
