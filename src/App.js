01c581c6f32cc7a209f2a2bed205c8038214ba8c
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-16 21:39:51 +0100

    Add color background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { useState } from 'react';
 
 export default function ExampleState() {
   // 1. Declare a state variable called "count", with
-  // the default value 0
+  // a default value
   const [hexcolor, generateHexColor] = useState("#fffff");
-  const randomHex = "#" + Math.floor(Math.random()*167777215 +' ' ).toString(16)
+  const randomHex = "#" + Math.floor(Math.random()*16777215).toString(16);
+  
   return (
     <>
       <h1>Color Generator</h1>
       {/* 2. Use the state variable */}
-      <div>
+      <div style={{backgroundColor: hexcolor}}>
         Generated Color: {hexcolor}
         </div>
       <br></br>
